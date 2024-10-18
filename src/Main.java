c724c370289de84e75db69a17fe5de5df2284302
Alfonso6692 <U20249301@utp.edu.pe>
2024-10-18 15:42:21 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.CDATASection;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -21,15 +23,17 @@ public class Main {
 
         ListaEnlazadaDoble listaEnlazadaDoble = new ListaEnlazadaDoble();
         ListaEnlazada listaEnlazada = new ListaEnlazada();
-        listaEnlazada.eliminar(1);
+        //listaEnlazada.eliminar(1);
         listaEnlazadaDoble.agregar(1);
         listaEnlazadaDoble.agregar(2);
         listaEnlazadaDoble.agregar(3);
         listaEnlazadaDoble.agregar(4);
-        listaEnlazadaDoble.eliminarInicio();//elimina el inicio "1"
-        listaEnlazadaDoble.eliminar();//elimina el ultimo "4"
-        listaEnlazadaDoble.agregarinicio(5);
-        listaEnlazadaDoble.tamaño();
+        //listaEnlazadaDoble.eliminarInicio();//elimina el inicio "1"
+        //istaEnlazadaDoble.eliminar();//elimina el ultimo "4"
+        //listaEnlazadaDoble.agregarinicio(5);
+        //listaEnlazadaDoble.tamaño();
+        System.out.println();
+
         listaEnlazadaDoble.verLista();
 
 
